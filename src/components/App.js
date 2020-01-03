25bd843cd17165e1088fb4adb522db2f465bbb1a
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-03 17:08:40 -0500

    App component done

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+import logo from '../images/logo.svg';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <img src={logo} className="App-logo" alt="logo-react" />
+        <Display />
+        <ButtonPanel />
+      </div>
+    );
+  }
+}
+
+export default App;
