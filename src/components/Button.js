2a10ccf3f52463473be73fefc5124beb9e44d248
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-06 12:06:27 -0500

    eslint solved for Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,19 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Button.css';
 
 class Button extends Component {
   render() {
+    const { name } = this.props;
     return (
-      <button className="Button">
-        {this.props.name}
+      <button className="Button" type="button">
+        {name}
       </button>
     );
   }
 }
 
 Button.propTypes = {
-  name: PropTypes.string
+  name: PropTypes.string,
+};
+
+Button.defaultProps = {
+  name: '',
 };
 
 export default Button;
