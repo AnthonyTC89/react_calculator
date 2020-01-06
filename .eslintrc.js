f14a4551a755c263cc6908c45b7a1ee133ce23b3
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-06 10:10:37 -0500

    eslint errors solved

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,12 @@ module.exports = {
   ],
   rules: {
     "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+  },
+  settings: {
+    "import/resolver": {
+      "node": {
+        "paths": ["src"]
+      }
+    }
   }
 };
