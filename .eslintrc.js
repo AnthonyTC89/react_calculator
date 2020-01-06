bc9bcf811f3e92522823e2e53868ae83a4ef581f
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-06 12:27:49 -0500

    .eslintrc.js updated #4

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,11 +26,11 @@ module.exports = {
     'react/jsx-filename-extension': [1, { 'extensions': ['.js', '.jsx'] }],
     'react/prefer-stateless-function': [0, { 'ignorePureComponents': true }],
   },
-  settings: {
-    'import/resolver': {
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
-        paths: ['./src']
+  "settings": {
+    "import/resolver": {
+      "node": {
+        "extensions": ['.js', '.jsx', '.ts', '.tsx'],
+        "paths": ['./src']
       }
     }
   }
