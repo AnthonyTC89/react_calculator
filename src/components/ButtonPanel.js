e8e20ecefcff8b7fa97eacd86df1482ab2aefd47
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-06 12:07:26 -0500

    eslint solved for ButtonPanel component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Button from './Button';
 import './ButtonPanel.css';
 
@@ -7,33 +7,33 @@ class ButtonPanel extends Component {
     return (
       <div className="ButtonPanel">
         <div className="Group-1">
-          <Button name="AC"/>
-          <Button name="+/-"/>
-          <Button name="%"/>
-          <Button name="÷"/>
+          <Button name="AC" />
+          <Button name="+/-" />
+          <Button name="%" />
+          <Button name="÷" />
         </div>
         <div className="Group-2">
-          <Button name="7"/>
-          <Button name="8"/>
-          <Button name="9"/>
-          <Button name="X"/>
+          <Button name="7" />
+          <Button name="8" />
+          <Button name="9" />
+          <Button name="X" />
         </div>
         <div className="Group-3">
-          <Button name="4"/>
-          <Button name="5"/>
-          <Button name="6"/>
-          <Button name="-"/>
+          <Button name="4" />
+          <Button name="5" />
+          <Button name="6" />
+          <Button name="-" />
         </div>
         <div className="Group-4">
-          <Button name="1"/>
-          <Button name="2"/>
-          <Button name="3"/>
-          <Button name="+"/>
+          <Button name="1" />
+          <Button name="2" />
+          <Button name="3" />
+          <Button name="+" />
         </div>
         <div className="Group-5">
-          <Button name="0"/>
-          <Button name="."/>
-          <Button name="="/>
+          <Button name="0" />
+          <Button name="." />
+          <Button name="=" />
         </div>
       </div>
     );
