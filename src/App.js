c9662b234482064cbf64f5d67a70ba971a83dd57
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-06 14:44:08 -0500

    annotations done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 /* eslint-disable import/no-unresolved */
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit
           <code>
