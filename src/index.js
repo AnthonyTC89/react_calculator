81479803cc13986eee556407405f3e2c531ca149
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-07 13:38:43 -0500

    branch updated

--- src/index.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable import/no-unresolved */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './components/App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
