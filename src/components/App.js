5b128daa75153faa4cf0474291470a9db716b1b9
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-07 15:07:11 -0500

    merge conflicts solved

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <Display />
+    <ButtonPanel />
+  </div>
+);
+
+export default App;
