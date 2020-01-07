e8f9f15fef55adf87469cb445bc21a885cfc6582
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-07 17:35:31 -0500

    operate.js done

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,1 +1,23 @@
 import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const x = Big(numberOne);
+  const y = Big(numberTwo);
+
+  switch (operation) {
+    case '+':
+      return x.plus(y);
+    case '-':
+      return x.minus(y);
+    case 'X':
+      return x.times(y);
+    case '÷':
+      return x.div(y);
+    case '%':
+      return x.div(100).times(y);
+    default:
+      break;
+  }
+};
+
+export default operate;
