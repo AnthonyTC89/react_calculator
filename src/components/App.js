7fd6dca1691151f0f4afab24ee85a86afc6734c2
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-07 19:53:39 -0500

    calculate.js added to App

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import './App.css';
+import calculate from '../logic/operate';
 
 const App = () => (
   <div className="App">
