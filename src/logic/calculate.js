bd2fa0384f16e01c415613935b06d6b9a258439d
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-07 19:52:05 -0500

    calculate.js done

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -1,23 +1,45 @@
+import operate from './operate';
+
 const calculate = ({data: {total, next, operation}, name}) => {
   switch (name) {
     case 'AC':
-      console.log(name);
+      total = 0; // nil
+      next = 0; // nil
+      operation = ''; // nil
       break;
     case '+/-': 
       total = total * -1;
       next = next * -1;
       break;
     case '%': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = '%';
       break;
     case '÷': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = '÷'
       break;
     case 'X': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = 'X'
       break;
     case '-': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = '-'
       break;
     case '+': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = '+'
       break;
     case '=': 
+      total = operate(total, next, operation);
+      next = '';
+      operation = '';
       break;
     default:
       break;
