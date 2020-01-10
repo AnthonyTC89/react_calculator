c56a19434e699b67060fcfd0c20adb418b2ef9c3
AnthonyTC89 <ptonyptc@gmail.com>
2020-01-09 19:30:29 -0500

    making the App component stateful

--- src/components/App.js
+++ src/components/App.js
@@ -2,31 +2,37 @@ import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import './App.css';
-// eslint-disable-next-line no-unused-vars
 import calculate from '../logic/calculate';
 
-const App = () => {
-  const [state, setState] = React.useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  const handleClick = (btnName) => {
-    const data = { ...state };
-    const result = calculate(data, btnName);
-    setState({
-      total: result.total,
-      next: result.next,
-      operation: result.operation,
-    });
-  };
-  return (
-    <div className="App">
-      <Display text={state.next || state.total || '0'} />
-      <ButtonPanel clickHandler={handleClick} />
-    </div>
-  );
-};
+  render() {
+    const handleClick = (btnName) => {
+      const data = { ...this.state };
+      const result = calculate(data, btnName);
+      this.setState({
+        total: result.total,
+        next: result.next,
+        operation: result.operation,
+      });
+    };
+    const { next, total } = this.state;
+    return (
+      <div className="App">
+        <Display text={next || total || '0'} />
+        <ButtonPanel clickHandler={handleClick} />
+      </div>
+    );
+  }
+
+}
 
 export default App;
